471d10a272c2b523e9db6ef83b50b62f52d365c9
Mathew <matymcleod@gmail.com>
2021-06-23 03:51:28 +0000

    updating comments

--- password.js
+++ password.js
@@ -1,9 +1,11 @@
 let args = process.argv.slice(2);
+// Stores and converts command line arguments to a string
 let input = args.toString();
 
 const obfuscate = function(param) {
   let newPass = [];
   for (let i = 0; i < param.length; i++) {
+    // These if and else if statements intercept param[i] and converts their values. FYI this doesn't work without the else statement
     if (param[i] === 'a') {
       newPass += '4';
     } else if (param[i] === 'o') {
